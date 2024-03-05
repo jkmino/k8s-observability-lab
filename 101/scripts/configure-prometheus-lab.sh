b2824e3b204c580c8bfd9cf24d226de1877f0423
Jose Camino <28641830+jkmino@users.noreply.github.com>
2024-03-04 19:08:31 -0500

    initial commit

---
+++ 101/scripts/configure-prometheus-lab.sh
@@ -0,0 +1,15 @@
+echo "---------- Configuring EKS  ----------"
+eksctl create cluster -f infra/cluster.yaml
+
+echo "---------- server metrics----------"
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+echo "----------  PROMETHEUS STACK ----------"
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+helm install prometheus prometheus-community/kube-prometheus-stack
+
+echo "---------- CONFIGURING TEST-API ----------"
+kubectl create namespace test-api
+
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # k8s-observability-lab
 
 ## Build cluster Using EKSCTL
-- eksctl config
+- eksctl creatio:
+
+```
+eksctl create cluster -f Infra/initial-cluster.yaml
+```
   
 ## Installing Tools
-- Grafana stack 
+- Grafana stack :
+```
+./scripts/configure-prometheus-lab.sh
+```
+
